71a5298a97b175c26693a50120b02a0407ecbe7d
MahmoudWesam <mahmoudamer95.bird@gmail.com>
2022-11-05 17:27:03 +0200

    added user auth endpoints

--- app/user/urls.py
+++ app/user/urls.py
@@ -3,4 +3,8 @@ from user import views
 
 app_name = "user"
 
-urlpatterns = [path("create/", views.CreateUserView.as_view(), name="create")]
+urlpatterns = [
+    path("create/", views.CreateUserView.as_view(), name="create"),
+    path("token/", views.CreateTokenViews.as_view(), name="token"),
+    path("me/", views.ManageUserViews.as_view(), name="me"),
+]
