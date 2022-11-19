b7723c1de869096ab298626ec5376de8a0080077
MahmoudWesam <mahmoudamer95.bird@gmail.com>
2022-11-19 18:05:03 +0200

    wip: added ability to create/update ingredients from recipe endpoints

--- app/recipe/serializers.py
+++ app/recipe/serializers.py
@@ -18,11 +18,11 @@ class IngredientSerializer(serializers.ModelSerializer):
 
 class RecipeSerializer(serializers.ModelSerializer):
     tags = TagSerializer(many=True, required=False)
-    # ingredients = IngredientSerializer(many=True, required=False)
+    ingredients = IngredientSerializer(many=True, required=False)
 
     class Meta:
         model = Recipe
-        fields = ["id", "title", "time_minutes", "price", "link", "tags"]
+        fields = ["id", "title", "time_minutes", "price", "link", "tags", "ingredients"]
         read_only_fields = ["id"]
 
     def _get_or_create_tag(self, tags, recipe):
@@ -31,20 +31,37 @@ class RecipeSerializer(serializers.ModelSerializer):
             tag_obj, _created = Tag.objects.get_or_create(user=auth_user, **tag)
             recipe.tags.add(tag_obj)
 
-    # Modifying default create funct since nested tags are treated as readonly fields
+    def _get_or_create_ingredient(self, ingredients, recipe):
+        auth_user = self.context["request"].user
+        for ingredient in ingredients:
+            ingredient_obj, _created = Ingredient.objects.get_or_create(
+                user=auth_user, **ingredient
+            )
+            recipe.ingredients.add(ingredient_obj)
+
+    # Modifying default create funct since nested tags/ingredients are treated as readonly fields
     def create(self, validated_data):
         tags = validated_data.pop("tags", [])
+        ingredients = validated_data.pop("ingredients", [])
         recipe = Recipe.objects.create(**validated_data)
+
         self._get_or_create_tag(tags, recipe)
+        self._get_or_create_ingredient(ingredients, recipe)
 
         return recipe
 
-    # Modifying default update to support modifying tags on patch
+    # Modifying default update to support modifying tags/ingredients on patch
     def update(self, instance, validated_data):
-        tags = validated_data.pop("tags", [])
+        tags = validated_data.pop("tags", None)
+        ingredients = validated_data.pop("ingredients", None)
+
         if tags is not None:
             instance.tags.clear()
             self._get_or_create_tag(tags, instance)
+
+        if ingredients is not None:
+            instance.ingredients.clear()
+            self._get_or_create_ingredient(ingredients, instance)
 
         for attr, value in validated_data.items():
             setattr(instance, attr, value)
